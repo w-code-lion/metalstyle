82114fe07d901435c7df6ad504741e7e06fd87e5
w-code-lion <w.code.lion@gmail.com>
2020-07-16 13:03:08 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('.header-slider').slick({
+  $('.header-slider-block').slick({
    infinite: true,
    arrows: false,
    dots: true,
@@ -9,7 +9,7 @@ $(document).ready(function(){
    speed: 700,
    
 
-responsive: [
+   responsive: [
     {
       breakpoint: 555,
       settings: {
@@ -21,57 +21,17 @@ responsive: [
  
   });
 
-});
+  //////////////////////////////////////////
 
-//////////////////////////////////////////
+  const menuToggle = document.querySelector('#menu__togle');
+  const mobileNavContainer = document.querySelector('#mobile__nav');
 
-const menuToggle = document.querySelector('#menu__togle');
-const mobileNavContainer = document.querySelector('#mobile__nav');
+  menuToggle.onclick = function(){
+      menuToggle.classList.toggle('menu-icon-active');
+      mobileNavContainer.classList.toggle('mobile-nav--active');
+  }
 
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavContainer.classList.toggle('mobile-nav--active');
-}
-
-///////////////////////////////////////////////
-
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 100, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1500, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: true, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
-});
-
-/////////////////////////////////////////////////
-
-$('.example').moreContent({
-  "shadow": true, // Добавляет в разметку блок с тенью
-  // Если нужно изменить разметку блока с тенью:
-  // tpl: {
-  //  shadow: '<div class="mrc-shadow"></div>'
-  // }
-});
-
-//////////////////////////////////////////////////
-
-
-$(document).ready(function(){
 
   $('.popup-link').magnificPopup({
 
@@ -88,7 +48,13 @@ $(document).ready(function(){
           }
       }
   });
-   
-});
 
-/////////////////////////////
+  $('.example').moreContent({
+    "shadow": true, // Добавляет в разметку блок с тенью
+    // Если нужно изменить разметку блока с тенью:
+    // tpl: {
+    //  shadow: '<div class="mrc-shadow"></div>'
+    // }
+  });
+
+});
---
+++ js/my-validate.js
@@ -0,0 +1,16 @@
+$("#myform").validate({
+  rules: {
+  	email: {
+  		required:true,
+  		minlength: 5
+  	},
+  	name: {
+  		required:true,
+  		minlength: 3
+  	},
+  	text: {
+  		required:true,
+  		minlength: 10
+  	}
+  }
+});
