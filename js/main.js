6ab2bf3f76306aab4763d50ed06611f3cf10c53b
w-code-lion <w.code.lion@gmail.com>
2020-07-28 11:45:18 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,58 +1,60 @@
 $(document).ready(function(){
 
-  $('.header-slider-block').slick({
-   infinite: true,
-   arrows: false,
-   dots: true,
-   autoplay: true,
-   autoplaySpeed: 3000,
-   speed: 700,
-   
-
-   responsive: [
-    {
-      breakpoint: 555,
-      settings: {
-        dots: false,
-      }
-    }
-  ]
-
- 
-  });
-
-  //////////////////////////////////////////
+  // BurgerMenu
 
   const menuToggle = document.querySelector('#menu__togle');
   const mobileNavContainer = document.querySelector('#mobile__nav');
 
   menuToggle.onclick = function(){
-      menuToggle.classList.toggle('menu-icon-active');
-      mobileNavContainer.classList.toggle('mobile-nav--active');
+	  menuToggle.classList.toggle('menu-icon-active');
+	  mobileNavContainer.classList.toggle('mobile-nav--active');
   }
-
   
 
-  $('.popup-link').magnificPopup({
+  // Header slickSlider
 
-      callbacks: {
-          open: function() {
-             $('.slider').slick({
-               infinite: true,
-               arrows: true,
-               dots: false,
-               autoplay: true,
-               autoplaySpeed: 3000,
-               speed: 700,
-             });
-          }
-      }
+  $('.header-slider-block').slick({
+	infinite: true,
+	arrows: false,
+	dots: true,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	speed: 700,
+	responsive: [
+	  {
+		breakpoint: 555,
+		settings: {
+		  dots: false
+		}
+	  }
+	]
   });
 
+  // Popup slickSlider
+  var slick = $('.slider').slick({
+	  infinite: true,
+	  arrows: true,
+	  dots: false,
+	  autoplay: true,
+	  autoplaySpeed: 3000,
+	  speed: 700
+  });
+
+  // fixed slider - magnificPopup
+  $('.decor__gallery-link, .spoler__gallery').click(function() {
+	 slick.slick('refresh');
+  });
+
+
+  // magnificPopup
+  $('.popup-link').magnificPopup();
+
+
+  // Toggle view content
   $('.example').moreContent({
-    "shadow": true, 
-    
- 
+	"shadow": true 
   });
+
+
 
 });
